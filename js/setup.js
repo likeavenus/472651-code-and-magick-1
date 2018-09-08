c67134c4caa90af851953ef6604ff149ef7a7f64
Rafael <rafael_guliev94@list.ru>
2018-09-08 16:21:59 +0300

    create warlocks function

--- js/setup.js
+++ js/setup.js
@@ -6,7 +6,7 @@ var surname = ['да Марья', 'Верон', 'Мирабелла', 'Валь�
 var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var WARLOCK_NAME = [
+var WARLOCKS_NAMES = [
   names[getRandomAttribute(0, 7)] + ' ' + surname[getRandomAttribute(0, 7)],
   names[getRandomAttribute(0, 7)] + ' ' + surname[getRandomAttribute(0, 7)],
   names[getRandomAttribute(0, 7)] + ' ' + surname[getRandomAttribute(0, 7)],
@@ -22,45 +22,53 @@ function getRandomAttribute(min, max) {
     rand = Math.floor(rand);
     return rand;
 };
+//попытка создать функцию, которая создает магов , что бы оптимизировать код ниже(нужно ли?)
+// var createWarlocks = function(warlocks) {
+//   for (var i = 0; i < WARLOCKS_NAMES.length; i++) {
+//     warlocks = WARLOCKS_NAMES[i];
+//   }
+//   return warlocks;
+// };
+// createWarlocks(warlocks);
 
 var warlocks = [
   {
-    name: WARLOCK_NAME[0],
+    name: WARLOCKS_NAMES[0],
     coatColor: coatColor[getRandomAttribute(0, 5)],
     eyesColor: eyesColor[getRandomAttribute(0, 4)]
   },
   {
-    name: WARLOCK_NAME[1],
+    name: WARLOCKS_NAMES[1],
     coatColor: coatColor[getRandomAttribute(0, 5)],
     eyesColor: eyesColor[getRandomAttribute(0, 4)]
   },
   {
-    name: WARLOCK_NAME[2],
+    name: WARLOCKS_NAMES[2],
     coatColor: coatColor[getRandomAttribute(0, 5)],
     eyesColor: eyesColor[getRandomAttribute(0, 4)]
   },
   {
-    name: WARLOCK_NAME[3],
+    name: WARLOCKS_NAMES[3],
     coatColor: coatColor[getRandomAttribute(0, 5)],
     eyesColor: eyesColor[getRandomAttribute(0, 4)]
   },
   {
-    name: WARLOCK_NAME[4],
+    name: WARLOCKS_NAMES[4],
     coatColor: coatColor[getRandomAttribute(0, 5)],
     eyesColor: eyesColor[getRandomAttribute(0, 4)]
   },
   {
-    name: WARLOCK_NAME[5],
+    name: WARLOCKS_NAMES[5],
     coatColor: coatColor[getRandomAttribute(0, 5)],
     eyesColor: eyesColor[getRandomAttribute(0, 4)]
   },
   {
-    name: WARLOCK_NAME[6],
+    name: WARLOCKS_NAMES[6],
     coatColor: coatColor[getRandomAttribute(0, 5)],
     eyesColor: eyesColor[getRandomAttribute(0, 4)]
   },
   {
-    name: WARLOCK_NAME[7],
+    name: WARLOCKS_NAMES[7],
     coatColor: coatColor[getRandomAttribute(0, 5)],
     eyesColor: eyesColor[getRandomAttribute(0, 4)]
   }
