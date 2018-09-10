91f94a215b685800614ce194bd8cf5a9977d56ac
Rafael <rafael_guliev94@list.ru>
2018-09-10 23:06:50 +0300

    fix renderName

--- js/setup.js
+++ js/setup.js
@@ -5,24 +5,20 @@ var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Валь
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var WARLOCKS_NAMES;
-
-var renderArrNames = function() {
-   WARLOCKS_NAMES = [names[getRandomAttribute(0, 7)] + ' ' + surnames[getRandomAttribute(0, 7)]];
-}
-
-var renderName = function (quantity) {
-  for (var i = 0; i < quantity; i++) {
-    renderArrNames();
-  }
-};
-renderName(9);
-
 function getRandomAttribute(min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   rand = Math.floor(rand);
   return rand;
 }
+
+var WARLOCKS_NAMES = [];
+
+var renderName = function (quantity) {
+  for (var i = 0; i < quantity; i++) {
+    WARLOCKS_NAMES[i] = names[getRandomAttribute(0, 7)] + ' ' + surnames[getRandomAttribute(0, 7)];;
+  }
+};
+renderName(5);
 
 var warlocks = [
   {
@@ -44,26 +40,6 @@ var warlocks = [
     name: WARLOCKS_NAMES[3],
     coatColor: coatColors[getRandomAttribute(0, 5)],
     eyesColor: eyesColors[getRandomAttribute(0, 4)]
-  },
-  {
-    name: WARLOCKS_NAMES[4],
-    coatColor: coatColors[getRandomAttribute(0, 5)],
-    eyesColor: eyesColors[getRandomAttribute(0, 4)]
-  },
-  {
-    name: WARLOCKS_NAMES[5],
-    coatColor: coatColors[getRandomAttribute(0, 5)],
-    eyesColor: eyesColors[getRandomAttribute(0, 4)]
-  },
-  {
-    name: WARLOCKS_NAMES[6],
-    coatColor: coatColors[getRandomAttribute(0, 5)],
-    eyesColor: eyesColors[getRandomAttribute(0, 4)]
-  },
-  {
-    name: WARLOCKS_NAMES[7],
-    coatColor: coatColors[getRandomAttribute(0, 5)],
-    eyesColor: eyesColors[getRandomAttribute(0, 4)]
   }
 ];
 
@@ -72,21 +48,20 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-var renderWarlock = function (warlock, quantity) {
-  for (var i = 0; i < quantity; i++) {
+var renderWarlock = function (warlock) {
     var warlockElement = similarWizardTemplate.cloneNode(true);
-    warlockElement.querySelector('.setup-similar-label').textContent = warlock[i].name;
-    warlockElement.querySelector('.wizard-coat').style.fill = warlock[i].coatColor;
-    warlockElement.querySelector('.wizard-eyes').style.fill = warlock[i].eyesColor;
+    warlockElement.querySelector('.setup-similar-label').textContent = warlock.name;
+    warlockElement.querySelector('.wizard-coat').style.fill = warlock.coatColor;
+    warlockElement.querySelector('.wizard-eyes').style.fill = warlock.eyesColor;
     return warlockElement;
-  }
 };
+renderWarlock(warlocks);
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < warlocks.length; i ++) {
   fragment.appendChild(renderWarlock(warlocks[i]));
 }
 similarList.appendChild(fragment);
-renderWarlock(warlocks, 4);
+
 
 document.querySelector('.setup').classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
